65ef21734323f6e8e73f1966110e18545572898d
Wacharaporn59 <59160130@go.buu.ac.th>
2019-09-22 23:03:33 +0700

    Binding 4

--- app/src/main/java/buu/informatics/c59160130/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/c59160130/aboutme/MainActivity.kt
@@ -3,18 +3,17 @@ package buu.informatics.c59160130.aboutme
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ContextMenu
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+
 import androidx.databinding.DataBindingUtil
 import buu.informatics.c59160130.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Wacharaporn Butsa")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,10 +28,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        this.myName = this@MainActivity.myName
+
     }
 
     private fun updateNickname(view: View) {
         binding.apply {
+
             nicknameEdit.visibility = View.VISIBLE
             nicknameTextView.visibility = View.GONE
 
@@ -48,12 +50,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             nicknameText.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
 
             val doneButton = doneButton
             doneButton.visibility = View.GONE
+            invalidateAll()
 
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
---
+++ app/src/main/java/buu/informatics/c59160130/aboutme/MyName.kt
@@ -0,0 +1,4 @@
+package buu.informatics.c59160130.aboutme
+
+data class MyName (var name: String = "", var nickname: String = "")
+
